c0f018df5c9b119daa1814237cb8b3063952f208
Moe Farid <moe@Moes-MacBook-Pro.local>
2021-04-30 16:42:39 -0400

    2

--- tests/example.test.js
+++ tests/example.test.js
@@ -4,13 +4,13 @@ var pageExample = "http://www.example.com/"
 
 describe('My First Puppeteer Test', ()=> {
 
-it('should launch the browser', async ()=> {
-    const browser = await puppeteer.launch({headless: false})
+it('should launch the browser', async () => {
+    const browser = await puppeteer.launch({headless: false, slowMo: 10, devtools: false })
     const page = await browser.newPage()
     await page.goto(pageExample)
+    await page.waitForTimeout(3000)
+    // await page.waitForSelector('header')
     await page.waitForSelector('h1')
-    await browser.close()
-
+    await browser.close();
 })
-
 })
