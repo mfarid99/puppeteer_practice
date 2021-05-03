c6fdf2fe15c82cfb2c2e98da5d38ef3d16cc7618
Moe Farid <moe@Moes-MacBook-Pro.local>
2021-05-03 12:22:43 -0400

    3

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,18 +1,37 @@
 const puppeteer = require('puppeteer')
+const expect = require('chai').expect
 
 var pageExample = "http://www.example.com/"
+var google = "https://www.google.com/"
+var devExpress = "https://devexpress.github.io/testcafe/example/"
+var messageA = "Howdy, my name is Moe"
 
 describe('My First Puppeteer Test', ()=> {
+  it('should launch the browser', async function () {
+      
 
-it('should launch the browser', async () => {
-    const browser = await puppeteer.launch({headless: false, slowMo: 10, devtools: false })
+
+    const browser = await puppeteer.launch ({
+      headless: true, 
+  
+     })
+
     const page = await browser.newPage()
     await page.goto(pageExample)
-    await page.waitForTimeout(3000)
-    // await page.waitForSelector('header')
-    await page.waitForSelector('h1')
-    await page.waitForSelector('h1')
-
-    await browser.close();
-})
+    const title = await page.title()
+    const url = await page.url()
+    const text = await page.$eval('h1', element => element.textContent)
+    //  console.log('Text in he is ' + text)
+     const par = await page.$eval('p', parCont => parCont.textContent)
+    //  console.log('Par content is ' + par)
+     const count = await page.$$eval('p', heat => heat.length)
+    //  console.log('The count is: ' + count)
+    expect(title).to.be.a('string', 'Example Domain')
+    expect(url).to.include('example.com')
+    expect(text).to.be.a('string', 'Example Domain')
+    expect(par).to.include('in literature')
+    expect(count).to.equal(2)
+    await browser.close()
+    
+  })
 })
